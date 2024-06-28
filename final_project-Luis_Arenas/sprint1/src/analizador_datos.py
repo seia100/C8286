00cff025a3113d38a57f1e06eda61a75ea05e19f
Luis Arenas <89529418+seia100@users.noreply.github.com>
2024-06-28 13:24:01 -0500

    Update analizador_datos.py

--- final_project-Luis_Arenas/sprint1/src/analizador_datos.py
+++ final_project-Luis_Arenas/sprint1/src/analizador_datos.py
@@ -1,6 +1,12 @@
+# analizador_datos.py
+
 from kafka import KafkaConsumer, KafkaProducer
 import json
 import redis
+import zmq
+from pymongo import MongoClient
+
+
 
 consumer = KafkaConsumer('processed_packets', bootstrap_servers=['kafka:9092'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
@@ -28,17 +34,44 @@ def detect_port_scan(packet):
             return True
     return False
 
+
+
+
+# Configuración de la conexión a MongoDB
+client = MongoClient("mongodb://database-service:27017/")
+db = client.ids
+collection = db.packet_data
+
+def analyze_packet(packet):
+    # Análisis básico del paquete y almacenamiento en MongoDB
+    if packet['protocol'] == 6:  # TCP
+        # Realizar análisis adicional si es necesario
+        collection.insert_one(packet)
+
+def start_data_analyzer():
+    context = zmq.Context()
+    socket = context.socket(zmq.PULL)
+    socket.bind("tcp://0.0.0.0:5555")
+
+    while True:
+        packet = socket.recv_json()
+        analyze_packet(packet)
+
+# if __name__ == "__main__":
+#     start_data_analyzer()
+
+
 # Implementar otras funciones de detección aquí
 
-for message in consumer:
-    packet = message.value
+# for message in consumer:
+#     packet = message.value
     
-    if detect_brute_force(packet):
-        alert = {"type": "brute_force", "details": packet}
-        producer.send('alerts', alert)
+#     if detect_brute_force(packet):
+#         alert = {"type": "brute_force", "details": packet}
+#         producer.send('alerts', alert)
     
-    if detect_port_scan(packet):
-        alert = {"type": "port_scan", "details": packet}
-        producer.send('alerts', alert)
+#     if detect_port_scan(packet):
+#         alert = {"type": "port_scan", "details": packet}
+#         producer.send('alerts', alert)
 
     # En caso ubiese llamar otras funciones de deteccion aqui 
